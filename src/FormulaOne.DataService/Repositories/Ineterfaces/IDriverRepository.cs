55224114c0549f9c2b642721e47ed59cc38591b1
Ahhmedshams <ahmedtaha757@gmail.com>
2023-09-01 21:45:46 +0300

    initial UnitOfWork

---
+++ src/FormulaOne.DataService/Repositories/Ineterfaces/IDriverRepository.cs
@@ -0,0 +1,13 @@
+﻿using FormulaOne.DataService.DbSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.DataService.Repositories.Ineterfaces
+{
+    public interface IDriverRepository : IGenericRepository<Driver>
+    {
+    }
+}
---
+++ src/FormulaOne.DataService/Repositories/Ineterfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.DataService.Repositories.Ineterfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<bool> AddAsync();
+        Task<bool> UpdateAsync();
+        Task<bool> DeleteAsync(Guid id);
+
+
+    }
+}
