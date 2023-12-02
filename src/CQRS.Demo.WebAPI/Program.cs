9f9be19af73c703d52a062987121b4a21e472bd5
Ahhmedshams <ahmedtaha757@gmail.com>
2023-12-02 19:41:17 +0200

    INIT HANGFIRE

--- src/CQRS.Demo.WebAPI/Program.cs
+++ src/CQRS.Demo.WebAPI/Program.cs
@@ -2,8 +2,12 @@
 using FormulaOne.DataService.Data;
 using FormulaOne.DataService.Repositories;
 using FormulaOne.DataService.Repositories.Ineterfaces;
+using Hangfire;
+using Hangfire.SQLite;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Globalization;
 using System.Reflection;
 
 namespace FormulaOne.WebAPI
@@ -15,7 +19,7 @@ namespace FormulaOne.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
+            var HangfireConnectionString = builder.Configuration.GetConnectionString("HangfireConnection");
             builder.Services.AddDbContext<ApplicationDbContext> (option=>option.UseSqlite(ConnectionString));
             // Add services to the container.
 
@@ -25,6 +29,22 @@ namespace FormulaOne.WebAPI
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
             builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            builder.Services.AddHangfireServer();
+
+
+            // Configer Hangfire Client 
+            builder.Services.AddHangfire(config =>
+            config
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSQLiteStorage(HangfireConnectionString)
+            );
+
+
+
+
 
             //Injecting The MediatR to our DI
             builder.Services.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly));
@@ -42,6 +62,15 @@ namespace FormulaOne.WebAPI
 
             app.UseAuthorization();
 
+            app.UseHangfireDashboard();
+            app.MapHangfireDashboard("/hangfire");
+
+            const int NUM = 1;
+            
+
+            RecurringJob.AddOrUpdate(() =>
+            
+            Console.WriteLine($"hello{NUM}"), Cron.Minutely);
 
             app.MapControllers();
 
